152ed17d5210bb74e8eefccd7dfd5acb6fdfb0bf
amo-ladoja <54277725+amo-ladoja@users.noreply.github.com>
2024-09-24 09:21:50 -0400

    lab 02 - exercise 2 -

--- Lab02/Olatunde_Mojisola_DA_F24_update_02.R
+++ Lab02/Olatunde_Mojisola_DA_F24_update_02.R
@@ -230,6 +230,113 @@ tf<- is.na(WRS.new)
   qqline(WRS.new)
   
   
-  #population dataset #
+  
+  #### Lab 02 Beginning population dataset ### Lab 02
+  
+### fitting a distribution beyond histograms for EPI.new
+  
+  attach(epi2024results06022024)
+  qqnorm(EPI.new); qqline(EPI.new)
+
+  ### Make a Q-Q plot against the generating distribution
+  x <- seq(20., 80., 1.0)
+  qqplot(qnorm(ppoints(200)), x)  
+  qqline(x)  
+  qqplot(qnorm(ppoints(200)),EPI.new)  
+  qqline(EPI.new)  
+  
+  ### Cumulative density function for EPI.new
+  
+  plot(ecdf(EPI.new), do.points=FALSE)
+  plot(ecdf(rnorm(1000, 45, 10)), do.points=FALSE)   
+  lines(ecdf(EPI.new))  
   
   
+  
+  ### fitting a distribution beyond histograms for APO.new
+  
+  qqnorm(APO.new); qqline(APO.new)
+  
+  ### ### Make a Q-Q plot against the generating distribution for APO.new
+  x <- seq(0,110,10)
+  qqplot(qnorm(ppoints(200)), x)  
+  qqline(x)
+  qqplot(qnorm(ppoints(200)),APO.new)  
+  qqline(APO.new)  
+  
+  ### Cumulative density function for APO.new
+  
+  plot(ecdf(APO.new), do.points=FALSE)
+  plot(ecdf(rnorm(1000, 45, 10)), do.points=FALSE)  
+  lines(ecdf(APO.new))  
+  
+  
+  ### fitting a distribution beyond histograms for WRS.new
+  qqnorm(WRS.new); qqline(WRS.new)
+
+  ### ### Make a Q-Q plot against the generatning distribution for WRS.new
+  x <- seq (0,110,10)
+  qqplot(qnorm(ppoints(200)), x)  
+  qqline(x)  
+  qqplot(qnorm(ppoints(200)),WRS.new) 
+  qqline(WRS.new)
+  
+  ## Cumulative density function for WRS.new
+  
+  plot(ecdf(WRS.new), do.points=FALSE)
+  plot(ecdf(rnorm(1000,45,10)), do.points=FALSE)  
+  lines(ecdf(WRS.new))  
+  
+  
+  ### Comparing distribution APO.new and APO.old -- Boxplot
+  
+  boxplot(APO.old, APO.new, names=c("APO.old","APO.new"))
+  
+  
+  
+  ###Lab 02 Exercise 02
+  
+  #Read in data population_newyear, epi.results, epi.weights
+  
+  population_newyear <- read.csv("~/ITWS-1100/DataAnalytics2024_Olatunde_Mojisola/Lab02/countries_populations_2023.csv")
+View(population_newyear)  
+
+epi.result <- epi2024results06022024
+epi.weights <- read.csv("~/ITWS-1100/DataAnalytics2024_Olatunde_Mojisola/Lab02/epi2024weights.csv")
+
+# Drop countries not in epi results
+populations <- population_newyear[-which(!population_newyear$Country %in% epi.result$country),]
+
+
+## sort populations by country
+populations <- populations[order(populations$Country),]
+
+#drop countries not in populations
+epi.result.sub <- epi.result[-which(!epi.result$country %in% populations$Country),]
+
+### sort epi results by country
+epi.result.sub <- epi.result.sub[order(epi.result.sub$country),]
+
+##only keep necessary columns
+epi.result.sub <- epi.result.sub[,c("country","EPI.old","EPI.new")]
+
+##convert population to nummeric
+epi.result.sub$population <- as.numeric(populations$Population)
+
+#compute population log base 10
+epi.result.sub$population_log <- log10(epi.result.sub$population)
+
+
+##Linear Model 
+
+lin.mod.epinew <- lm(EPI.new~population_log,epi.result.sub)
+plot(EPI.new~population_log)
+abline(lin.mod.epinew)
+summary(lin.mod.epinew)
+plot(lin.mod.epinew)
+
+
+##ggplot
+ggplot(epi.result.sub, aes(x = population_log, y=EPI.new)) +
+  geom_point() +
+  stat_smooth(method ="lm")
